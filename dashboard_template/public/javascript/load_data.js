1f588065c761091bf24e4c0fe3db2efa9c867d3e
Xavier Cobos <xcobos@espol.edu.ec>
2023-11-01 10:33:15 -0500

    Update load_data.js

--- dashboard_template/public/javascript/load_data.js
+++ dashboard_template/public/javascript/load_data.js
@@ -92,10 +92,56 @@ let cargarOpenMeteo = () => {
   }
 
 
+  let cargarOpenMeteo2 = () => {
+
+    //URL que responde con la respuesta a cargar
+    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=dewpoint_2m,precipitation_probability&timezone=auto'; 
+  
+    fetch( URL )
+    .then(responseText => responseText.json())
+    .then(responseJSON => {
+      
+    //Respuesta en formato JSON
+
+    //Referencia al elemento con el identificador plot
+    let plotRef = document.getElementById('plot2');
+
+    //Etiquetas del gráfico
+    let labels = responseJSON.hourly.time;
+
+    //Etiquetas de los datos
+    let data = responseJSON.hourly.precipitation_probability;
+    let data2 = responseJSON.hourly.dewpoint_2m;
+
+    //Objeto de configuración del gráfico
+    let config = {
+        type: 'line',
+        data: {
+          labels: labels, 
+          datasets: [
+            {
+              label: 'Precipitation Probability',
+              data: data, 
+            },
+            {
+                label: 'dewpoint [2m]',
+                data: data2,
+            }
+          ]
+        }
+      };
+
+    //Objeto con la instanciación del gráfico
+    let chart2  = new Chart(plotRef, config);
+
+  })
+    .catch(console.error); 
+  }
+
   cargarPrecipitacion()
   cargarFechaActual()
   cargarOpenMeteo()
-
+  cargarOpenMeteo2()
   
   
 
