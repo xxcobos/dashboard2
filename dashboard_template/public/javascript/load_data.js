0f00ddcc3d9506dcf4452d90b98112b5c3fbb3a0
Xavier Cobos <xcobos@espol.edu.ec>
2023-11-06 10:57:32 -0500

    Update

--- dashboard_template/public/javascript/load_data.js
+++ dashboard_template/public/javascript/load_data.js
@@ -138,11 +138,116 @@ let cargarOpenMeteo = () => {
     .catch(console.error); 
   }
 
+
+
+
+
+
+  let parseXML = (responseText) => {
+  
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(responseText, "application/xml");
+
+
+    // Referencia al elemento `#forecastbody` del documento HTML
+
+    let forecastElement = document.querySelector("#forecastbody")
+    forecastElement.innerHTML = ''
+
+    // Procesamiento de los elementos con etiqueta `<time>` del objeto xml
+    let timeArr = xml.querySelectorAll("time")
+
+    timeArr.forEach(time => {
+        
+        let from = time.getAttribute("from").replace("T", " ")
+
+        let humidity = time.querySelector("humidity").getAttribute("value")
+        let windSpeed = time.querySelector("windSpeed").getAttribute("mps")
+        let precipitation = time.querySelector("precipitation").getAttribute("probability")
+        let pressure = time.querySelector("pressure").getAttribute("value")
+        let cloud = time.querySelector("clouds").getAttribute("value")
+
+        let template = `
+            <tr>
+                <td>${from}</td>
+                <td>${humidity}</td>
+                <td>${windSpeed}</td>
+                <td>${precipitation}</td>
+                <td>${pressure}</td>
+                <td>${cloud}</td>
+            </tr>
+        `
+
+        //Renderizando la plantilla en el elemento HTML
+        forecastElement.innerHTML += template;
+    })
+
+  
+  }
+
+
+
+
+
+
+
+  
+  //Callback
+  let selectListener = async (event) => {
+  
+    let selectedCity = event.target.value
+    let cityStorage = localStorage.getItem(selectedCity);
+    
+    if(cityStorage == null){
+      try {
+
+        //API key
+        let APIkey = '544642d69e67e6a5295680c3b7ffb831'
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&mode=xml&appid=${APIkey}`
+
+        let response = await fetch(url)
+        let responseText = await response.text()
+        
+        await parseXML(responseText)
+  
+      } catch (error) {
+        console.log(error)
+      }
+    
+    }else{
+      parseXML(cityStorage)
+    }
+    
+    
+   }
+  
+
+
+
+
+
+
+  let loadForecastByCity = () => {
+  
+    //Handling event
+ 
+    let selectElement = document.querySelector("select")
+    selectElement.addEventListener("change", selectListener)
+  
+  }
+  
+
+
+
+
+
+  loadForecastByCity()
   cargarPrecipitacion()
   cargarFechaActual()
   cargarOpenMeteo()
   cargarOpenMeteo2()
-  
+  selectListener()
+  parseXML()
   
 
  
